47de7435b6f29974cb75c8568eeec5fbdbb4346d
GoldJay <jasonlynch10@hotmail.com>
2020-05-15 12:01:35 +0100

    Adjusted game audio

--- RoboCat/Src/SoundManager.cpp
+++ RoboCat/Src/SoundManager.cpp
@@ -23,7 +23,7 @@ void SoundManager::LoadSoundFromFile(sf::Sound &p_sound, sf::SoundBuffer &p_buff
 	if (p_buffer.loadFromFile(p_file))
 	{
 		p_sound.setBuffer(p_buffer);
-		p_sound.setVolume(5);
+		p_sound.setVolume(1);
 	}
 }
 
@@ -31,7 +31,7 @@ void SoundManager::LoadMusicFromFile(sf::Music &p_music, string p_file)
 {
 	p_music.openFromFile(p_file);
 	p_music.setLoop(true);
-	p_music.setVolume(5);
+	p_music.setVolume(1);
 }
 
 void SoundManager::PlayMusic()
